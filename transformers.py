a14ccba87b89215edd9cf3fdea9c1a3dac5bf018
Lukas <lukassnoek@gmail.com>
2015-12-30 15:58:54 +0100

    Implement first scikit-learn compatible transformers!

--- transformers.py
+++ transformers.py
@@ -1,31 +1,33 @@
 """
 Module with transformer-classes following the scikit-learn API
-Rewritten code from my MSc thesis project.
+Rewritten code from my MSc thesis project (github.com/lukassnoek/MSc_thesis)
 """
 
 from __future__ import division
 import numpy as np
+import nibabel as nib
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_selection import f_classif
 
 __author__ = 'Lukas Snoek'
 
 
 class Subject:
     """
-    Contains (meta) data/patterns about single-trial fMRI-BOLD data.
+    Contains meta data about single-trial fMRI-BOLD data.
     """
 
-    def __init__(self, X, y, subject_name, mask_name, mask_index, mask_shape,
-                 mask_threshold, class_labels):
+    def __init__(self, class_labels, subject_name, mask_name, mask_index, mask_shape,
+                 mask_threshold):
 
         # Meta-data
         self.subject_name = subject_name
-        self.class_labels = np.asarray(class_labels)
+        self.class_labels = class_labels
         self.class_names = np.unique(self.class_labels)
 
         # Primary data
-        self.X = X
-        self.y = y
+        self.X = None  # should be set in pipeline from hdf5 file
+        self.y = None  # should be set in pipeline using LabelEncoder
 
         # Mask info
         self.mask_name = mask_name              # Name of nifti-file
@@ -34,15 +36,32 @@ class Subject:
         self.mask_threshold = mask_threshold
 
         # Information about condition/class
-        self.n_class = len(np.unique(y))
-        self.n_inst = [np.sum(cls == y) for cls in np.unique(y)]
+        self.n_class = len(self.class_names)
+        self.n_inst = [np.sum(cls == class_labels) for cls in self.class_names]
 
-        self.class_idx = [y == cls for cls in np.unique(y)]
-        self.trial_idx = [np.where(y == cls)[0] for cls in np.unique(y)]
+        self.class_idx = [class_labels == cls for cls in self.class_names]
+        self.trial_idx = [np.where(class_labels == cls)[0] for cls in self.class_names]
 
 
-class AverageSubject:
-    pass
+class AverageSubject(Subject):
+    """
+    Will initalize a Subject object which contains the class-average patterns
+    of a series of subjects, instead of a set of within-subject single-trial
+    patterns.
+    """
+
+    def __init__(self):
+        pass
+
+class ConcatenatedSubject(Subject):
+    """
+    Will initialize a Subject object which contains a set of single-trial
+    patterns concatenated across multiple subjects, yielding a matrix of
+    (trials * subjects) x features.
+    """
+
+    def __init__(self):
+        pass
 
 
 """
@@ -52,19 +71,55 @@ because they're not data-driven and thus do not need to be cross-validated.
 """
 
 
-class AverageRegionTransformer():
+class AverageRegionTransformer(BaseEstimator, TransformerMixin):
+    """
+    Computes the average from different regions from a given parcellation
+    and returns those as features for X.
+    """
+    def __init__(self, mask_list, orig_mask_index, orig_mask_threshold, orig_shape):
+        self.mask_list = mask_list
+        self.orig_mask = orig_mask_index
+        self.orig_shape = orig_shape
+        self.orig_threshold = orig_mask_threshold
+
+    def fit(self, X):
+        return self
+
+    def transform(self, X):
+        """
+        Transforms features from X (voxels) to region-average features from X.
+
+        Args:
+            X: array of trials x features (voxels in MNI152 2mm space)
+
+        Returns:
+            X_new: array of trials x region averages (of length mask_list)
+        """
+
+        X_new = np.zeros((X.shape[0], len(self.mask_list)))
+
+        for i, mask in enumerate(self.mask_list):
+            roi_idx = nib.load(mask).get_data() > self.orig_threshold
+            overlap = roi_idx.astype(int).ravel() + self.orig_mask.astype(int)
+            region_av = np.mean(X[:, (overlap == 2)[self.orig_mask]], axis=1)
+            X_new[:, i] = region_av
+
+        return X_new
+
+
+class PCAfilter(BaseEstimator, TransformerMixin):
+    """
+    Will implement a way to regress out a specified number of PCA components,
+    which are assumed to be noise components.
+    """
     pass
 
 
-class PCAfilter():
-    pass
-
-
-class Demean():
-    pass
-
-
-class SpatialFilter():
+class SpatialFilter(BaseEstimator, TransformerMixin):
+    """
+    Will implement a spatial filter that high-passes a 3D pattern of
+    voxel weights.
+    """
     pass
 
 
@@ -76,73 +131,37 @@ applied (crossvalidated) on the test-set.
 
 
 class AnovaCutoff(BaseEstimator, TransformerMixin):
-    pass
+    """
+    This class implements a ANOVA-based feature selection based on a
+    cutoff (minimal value) for the returned F-values.
+    """
+
+    def __init__(self, cutoff):
+        self.cutoff = cutoff
+
+    def fit(self, X, y):
+        f, _ = f_classif(X, y)
+        self.f_ = f
+        self.idx_ = f > self.cutoff
+        return self
+
+    def transform(self, X):
+        return X[:, self.idx_]
 
 
 class ClusterThreshold(BaseEstimator, TransformerMixin):
+    """
+    Will implement a cluster-average feature selection as described in
+    my master thesis (github.com/lukassnoek/MSc_thesis).
+    """
     pass
 
 
 class AveragePatterns(BaseEstimator, TransformerMixin):
+    """
+    Reduces the set of features to its average.
+    """
     pass
 
-
-
-
-
-class SelectAboveZvalue(BaseEstimator, TransformerMixin):
-    """ Selects features based on normalized differentation scores above cutoff
-
-    Feature selection method based on largest univariate differences;
-    similar to sklearn's univariate feature selection (with fclassif), but
-    selects all (normalized) difference scores, which are computed as the
-    mean absolute difference between feature values averaged across classes*,
-    above a certain z-value.
-    * e.g. mean(abs(mean(A)-mean(B), mean(A)-mean(C), mean(B)-mean(C)))
-
-    Works for any amount of classes.
-
-    Attributes:
-        zvalue (float/int): cutoff/lowerbound for normalized diff score
-
-    """
-
-    def __init__(self, zvalue):
-        self.zvalue = zvalue
-        self.idx_ = None
-        self.zvalues_ = None
-
-    def fit(self, X, y):
-        """ Performs feature selection on array of n_samples, n_features
-        Args:
-            X: array with n_samples, n_features
-            y: labels for n_samples
-        """
-
-        n_class = np.unique(y).shape[0]
-        n_features = X.shape[1]
-
-        av_patterns = np.zeros((n_class, n_features))
-
-        # Calculate mean patterns
-        for i in xrange(n_class):
-            av_patterns[i, :] = np.mean(X[y == np.unique(y)[i], :], axis=0)
-
-        # Create difference vectors, z-score standardization, absolute
-        comb = list(itls.combinations(range(1, n_class + 1), 2))
-        diff_patterns = np.zeros((len(comb), n_features))
-        for i, cb in enumerate(comb):
-            x = av_patterns[cb[0] - 1] - av_patterns[cb[1] - 1, :]
-            diff_patterns[i, :] = np.abs((x - x.mean()) / x.std())
-
-        mean_diff = np.mean(diff_patterns, axis=0)
-        self.idx_ = mean_diff > self.zvalue
-        self.zvalues_ = mean_diff
-
-        return self
-
-    def transform(self, X):
-        """ Transforms n_samples, n_features array """
-        return X[:, self.idx_]
 
 
