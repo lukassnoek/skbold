d28416d74966303b0908a169a3fcdd196d3e9075
Lukas Snoek <lukassnoek@gmail.com>
2016-03-24 15:46:40 +0100

    Fix import module issue (again)

--- skbold/utils/__init__.py
+++ skbold/utils/__init__.py
@@ -6,8 +6,8 @@ from .datahandler import DataHandler
 from .mvp_results import MvpResults
 from .mvp_average_results import MvpAverageResults
 from .sort_numbered_list import sort_numbered_list
-from .convert2mni import convert2mni
-from .convert2epi import convert2epi
+from .convert_to_mni import convert2mni
+from .convert_to_epi import convert2epi
 
 __all__ = ['DataHandler', 'MvpResults', 'MvpAverageResults',
            'sort_numbered_list', 'convert2epi', 'convert2mni']
