a6b9ace4ec7bf5ae4a7c94ed544c479f1116c6eb
Lukas Snoek <lukassnoek@gmail.com>
2016-03-24 15:42:28 +0100

    Fix classifier import issue

--- skbold/classifiers/__init__.py
+++ skbold/classifiers/__init__.py
@@ -1,3 +1,8 @@
 # Author: Lukas Snoek [lukassnoek.github.io]
 # Contact: lukassnoek@gmail.com
 # License: 3 clause BSD
+
+from .roi_stacking_classifier import RoiStackingClassifier
+from .roi_voting_classifier import RoiVotingClassifier
+
+__all__ = ['RoiStackingClassifier', 'RoiVotingClassifier']
