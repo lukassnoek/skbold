2efa40906d9ab0c0f46182c9914ffe9b3822eac7
Lukas Snoek <lukassnoek@gmail.com>
2016-04-05 15:15:02 +0200

    Fix roi bug

--- skbold/classifiers/roi_voting_classifier.py
+++ skbold/classifiers/roi_voting_classifier.py
@@ -14,7 +14,7 @@ import numpy as np
 import os.path as op
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.svm import SVC
-from skbold.data.rois import harvard_oxford as roi
+from skbold.data.ROIs import harvard_oxford as roi
 from skbold.transformers import RoiIndexer, MeanEuclidean
 from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.preprocessing import StandardScaler
