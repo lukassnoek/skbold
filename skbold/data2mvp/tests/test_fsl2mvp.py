a6816881a7d7d055512daefda94cd2adb33572f0
Lukas Snoek <lukassnoek@gmail.com>
2016-04-14 13:43:50 +0200

    Fix mvp bug

--- skbold/data2mvp/tests/test_fsl2mvp.py
+++ skbold/data2mvp/tests/test_fsl2mvp.py
@@ -12,13 +12,12 @@ def test_fsl2mvp():
 
     run1 = op.join(testdata_path, 'run1.feat')
     run2 = op.join(testdata_path, 'run2.feat')
-
     true_labels = ['actie', 'actie', 'actie',
                    'interoception', 'interoception', 'interoception',
                    'situation', 'situation', 'situation']
 
     mvp_dir = op.join(testdata_path, 'mvp_data')
-
+    shutil.rmtree(mvp_dir)
     for r in [run1, run2]:
         fsl2mvp = Fsl2mvp(r, mask_threshold=0, beta2tstat=True,
                           ref_space='epi', mask_path=None, remove_class=[])
@@ -28,6 +27,7 @@ def test_fsl2mvp():
 
         data_file = op.join(mvp_dir, 'test_data_data_%s.hdf5' % op.basename(r).split('.')[0])
         hdr_file = op.join(mvp_dir, 'test_data_header_%s.pickle' % op.basename(r).split('.')[0])
+
         assert(op.exists(data_file))
         assert(op.exists(hdr_file))
 
@@ -43,7 +43,9 @@ def test_fsl2mvp():
 
     assert(data.shape[0] == len(true_labels) * 2)
 
+    shutil.rmtree(mvp_dir)
     for r in [run1, run2]:
+
         fsl2mvp = Fsl2mvp(r, mask_threshold=0, beta2tstat=True,
                           ref_space='mni', mask_path=None, remove_class=[])
         fsl2mvp.glm2mvp()
