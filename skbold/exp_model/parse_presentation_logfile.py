268d8b3c2862bc3fb7fdc11af3fb260d448a0bcc
Lukas Snoek <lukassnoek@gmail.com>
2016-05-03 09:59:34 +0200

    Fix bug when codes is list of strings (e.g. [['J', 'P'], ['C', 'A']]) and make function parse_logfile_crawler initialize a ParsePresentationLogfile() object to remove redundant code

--- skbold/exp_model/parse_presentation_logfile.py
+++ skbold/exp_model/parse_presentation_logfile.py
@@ -103,16 +103,13 @@ class PresentationLogfileCrawler(object):
             if type(code) == str:
                 code = [code]
 
-#            if len(code) > 1:
-#               code = [[c] for c in code if type(c) != list]
-
             if len(code) > 1:
                 # Code is list of possibilities
                 if all(isinstance(c, int) for c in code):
                     idx = df['Code'].isin(code)
 
                 elif all(isinstance(c, str) for c in code):
-                    idx = [x in code for x in df['Code'] if type(x) == str]
+                    idx = [any(c in x for c in code) if isinstance(x, str) else False for x in df['Code']]
                     idx = np.array(idx)
 
             elif len(code) == 1 and type(code[0]) == str:
@@ -139,7 +136,7 @@ class PresentationLogfileCrawler(object):
                 to_write['Duration'] = [self.con_duration[i]] * idx.sum()
 
             to_write['Weight'] = np.ones((np.sum(idx), 1))
-            to_write['Name'] = [con_names[i] + '_%i' % (j + 1) for j in range(idx.sum())]
+            to_write['Name'] = [self.con_names[i] + '_%i' % (j + 1) for j in range(idx.sum())]
 
             if self.con_design[i] == 'univar':
                 trial_names.append(to_write['Name'].tolist())
@@ -155,11 +152,11 @@ class PresentationLogfileCrawler(object):
             if self.write_bfsl:
                 self._write_bfsl(i)
 
-        subject_info = Bunch(conditions=con_names,
+        subject_info = Bunch(conditions=self.con_names,
                              onsets=trial_onsets,
                              durations=trial_durations,
                              amplitudes=None,
-                             regressor_names=con_names,
+                             regressor_names=self.con_names,
                              regressors=None)
 
         return subject_info
@@ -196,7 +193,7 @@ class PresentationLogfileCrawler(object):
 
 
 def parse_presentation_logfile(in_file, con_names, con_codes, con_design=None,
-                               pulsecode=30, write_bfsl=False, verbose=True):
+                               con_duration=None, pulsecode=30, write_bfsl=False, verbose=True):
     """
     Parses a Presentation-logfile and extracts stimulus/event times and
     durations given their corresponding codes in the logfile.
@@ -206,107 +203,14 @@ def parse_presentation_logfile(in_file, con_names, con_codes, con_design=None,
     see custom piopfaces logfile crawler for example
     """
 
-    from nipype.interfaces.base import Bunch
-    import pandas as pd
-    import numpy as np
-    import os
-    import glob
+    from skbold.exp_model import PresentationLogfileCrawler
 
-    if type(in_file) == str:
-        in_file = [in_file]
+    plc = PresentationLogfileCrawler(in_file=in_file, con_names=con_names,
+                                     con_codes=con_codes, con_design=con_design,
+                                     con_duration=con_duration, pulsecode=pulsecode,
+                                     write_bfsl=True, verbose=False)
 
-    subject_info_files = []
-
-    for f in in_file:
-
-        if verbose:
-            print('Processing %s' % f)
-
-        base_dir = os.path.dirname(f)
-        _ = [os.remove(x) for x in glob.glob(os.path.join(base_dir, '*.bfsl'))]
-
-        if not con_design:
-            con_design = ['univar'] * len(con_names)
-
-        df = pd.read_table(f, sep='\t', skiprows=3, header=0,
-                           skip_blank_lines=True)
-
-        # Convert to numeric and drop all rows until first pulse
-        df['Code'] = df['Code'].astype(str)
-        df['Code'] = [np.float(x) if x.isdigit() else x for x in df['Code']]
-        pulse_idx = np.where(df['Code'] == pulsecode)[0]
-
-        if len(pulse_idx) > 1:
-            pulse_idx = int(pulse_idx[0])
-
-        df = df.drop(range(pulse_idx))
-
-        # Clean up unnecessary columns
-        df.drop(['Uncertainty', 'Subject', 'Trial', 'Uncertainty.1', 'ReqTime',
-                 'ReqDur', 'Stim Type', 'Pair Index'], axis=1, inplace=True)
-
-        # pulse_t = absolute time of first pulse
-        pulse_t = df['Time'][df['Code'] == pulsecode].iloc[0]
-        df['Time'] = (df['Time']-float(pulse_t)) / 10000.0
-        df['Duration'] = df['Duration'] / 10000.0
-
-        trial_names = []
-        trial_onsets = []
-        trial_durations = []
-
-        for i, code in enumerate(con_codes):
-            to_write = pd.DataFrame()
-            if len(code) > 1:
-                if type(code[0]) == int:
-                    idx = df['Code'].isin(code)
-            elif len(code) == 1 and type(code[0]) == str:
-                idx = [code[0] in x if type(x) == str else False for x in df['Code']]
-                idx = np.array(idx)
-            else:
-                idx = df['Code'] == code
-
-            # Generate dataframe with time, duration, and weight given idx
-            to_write['Time'] = df['Time'][idx]
-            to_write['Duration'] = df['Duration'][idx]
-            to_write['Duration'] = [np.round(x, decimals=2) for x in to_write['Duration']]
-            to_write['Weight'] = np.ones((np.sum(idx), 1))
-            to_write['Name'] = [con_names[i] + '_%i' % (j+1) for j in range(idx.sum())]
-
-            if con_design[i] == 'univar':
-                trial_names.append(to_write['Name'].tolist())
-                trial_onsets.append(to_write['Time'].tolist())
-                trial_durations.append(to_write['Duration'].tolist())
-            elif con_design[i] == 'multivar':
-                _ = [trial_names.append([x]) for x in to_write['Name'].tolist()]
-                _ = [trial_onsets.append([x]) for x in to_write['Time'].tolist()]
-                _ = [trial_durations.append([x]) for x in to_write['Duration'].tolist()]
-
-            if write_bfsl:
-
-                if con_design[i] == 'univar':
-                    to_write.drop('Name', axis=1, inplace=True)
-                    name = os.path.join(base_dir, '%s_00%i.bfsl' % (con_names[i], (i+1)))
-                    to_write = to_write[['Time', 'Duration', 'Weight']]
-                    to_write.to_csv(name, sep='\t', index=False, header=False)
-
-                elif con_design[i] == 'multivar':
-
-                    for ii, (index, row) in enumerate(to_write.iterrows()):
-                        ev_name = '%s.bfsl' % row['Name']
-                        name = os.path.join(base_dir, ev_name)
-                        df_tmp = pd.DataFrame({'Time': row['Time'],
-                                               'Duration': row['Duration'],
-                                               'Weight': row['Weight']}, index=[0])
-                        df_tmp = df_tmp[['Time', 'Duration', 'Weight']]
-                        df_tmp.to_csv(name, index=False, sep='\t', header=False)
-
-        subject_info = Bunch(conditions=con_names,
-                             onsets=trial_onsets,
-                             durations=trial_durations,
-                             amplitudes=None,
-                             regressor_names=con_names,
-                             regressors=None)
-        subject_info_files.append(subject_info)
+    subject_info_files = plc.parse()
 
     return subject_info_files
 
