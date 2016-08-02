e01ee241c78aff86855b29ac345101c43d3fb108
Lukas Snoek <lukassnoek@gmail.com>
2016-08-02 18:22:07 +0200

    Attempt to fix mock imports for rtd

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,7 @@ import mock
 
 #for mod_name in MOCK_MODULES:
 #   sys.modules[mod_name] = mock.Mock()
+# test
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
