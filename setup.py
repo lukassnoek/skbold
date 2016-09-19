83ebddc759bfb1eedc460da3208d2b0ca17df230
Lukas Snoek <lukassnoek@gmail.com>
2016-09-19 15:02:11 +0200

    Remove tox, because it's too complicated in combination with anaconda

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 import skbold
-import sys
 from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
 
 REQUIREMENTS = [
     'scipy>=0.17',
@@ -21,24 +19,6 @@ VERSION = skbold.__version__
 def readme():
     with open('README.rst') as f:
         return f.read()
-
-class Tox(TestCommand):
-    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.tox_args = None
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-    def run_tests(self):
-        #import here, cause outside the eggs aren't loaded
-        import tox
-        import shlex
-        args = self.tox_args
-        if args:
-            args = shlex.split(self.tox_args)
-        tox.cmdline(args=args)
 
 setup(
     name='skbold',
@@ -62,6 +42,4 @@ setup(
     install_requires=REQUIREMENTS,
     scripts=['bin/check_mc_output'],
     include_package_data=True,
-    tests_require=['tox'],
-    cmdclass={'test': Tox},
     zip_safe=False)
