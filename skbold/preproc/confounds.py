cff500583eb74957112eb457f6bd8d80b6b1be33
Lukas Snoek <lukassnoek@gmail.com>
2017-10-30 17:21:23 +0100

    Refactor such that it automatically infers fit_idx

--- skbold/preproc/confounds.py
+++ skbold/preproc/confounds.py
@@ -10,7 +10,7 @@ from sklearn.base import BaseEstimator, TransformerMixin
 class ConfoundRegressor(BaseEstimator, TransformerMixin):
     """ Fits a confound onto each feature in X and returns residuals."""
 
-    def __init__(self, confound, fit_idx, cross_validate=True,
+    def __init__(self, confound, X, cross_validate=True,
                  stack_intercept=True):
         """ Regresses out a variable (confound) from each feature in X.
 
@@ -36,28 +36,27 @@ class ConfoundRegressor(BaseEstimator, TransformerMixin):
             Array with weights for the confound(s).
         """
 
-        confound = np.array(confound)
-
-        if confound.ndim == 1 or stack_intercept:
-            intercept = np.ones(confound.shape[0])
-            confound = np.column_stack((intercept, confound))
-
         self.confound = confound
         self.cross_validate = cross_validate
-        self.fit_idx = fit_idx
+        self.X = X
+        self.stack_intercept = stack_intercept
         self.weights_ = None
 
     def fit(self, X, y=None):
 
-        confound = self.confound[self.fit_idx]
-        weights = np.zeros((X.shape[1], confound.shape[1]))
+        if self.confound.ndim == 1 and self.stack_intercept:
+            intercept = np.ones(self.confound.shape[0])
+            self.confound = np.column_stack((intercept, self.confound))
 
+        confound = self.confound
+        fit_idx = np.in1d(self.X, X).reshape(self.X.shape).sum(axis=1) == self.X.shape[1]
+        confound = confound[fit_idx, :]
+        weights = np.zeros((X.shape[1], confound.shape[1]))
         for i in range(X.shape[1]):
             b, _, _, _ = np.linalg.lstsq(confound, X[:, i])
             weights[i, :] = b
 
         self.weights_ = weights
-        self.X_used_for_fit = X
         return self
 
     def transform(self, X):
@@ -73,19 +72,31 @@ class ConfoundRegressor(BaseEstimator, TransformerMixin):
             ndarray with confound-regressed features
         """
 
-        if np.all(self.X_used_for_fit == X):
-            confound = self.confound[self.fit_idx]
-        else:
-            tmp_idx = np.ones(self.confound.shape[0], dtype=bool)
-            tmp_idx[self.fit_idx] = False
-            confound = self.confound[tmp_idx]
+        if not self.cross_validate:
+            self.fit(X)
 
-            if not self.cross_validate:
-                self.fit_idx = tmp_idx
-                self.fit(X)
-        
+        fit_idx = np.in1d(self.X, X).reshape(self.X.shape).sum(axis=1) == self.X.shape[1]
+        confound = self.confound[fit_idx]
         X_new = np.zeros_like(X)
         for i in range(X.shape[1]):
             X_new[:, i] = X[:, i] - np.squeeze(confound.dot(self.weights_[i, :]))
 
         return X_new
+
+
+if __name__ == '__main__':
+
+    from sklearn.pipeline import Pipeline
+    from sklearn.svm import SVC
+    from sklearn.model_selection import cross_val_score, StratifiedKFold
+    import numpy as np
+
+    X = np.random.normal(0, 1, (100, 500))
+    y = np.repeat([0, 1], 50)
+    c = np.random.normal(0, 1, 100)
+    skf = StratifiedKFold(10)
+
+    pipeline = [('cr', ConfoundRegressor(c, X, cross_validate=False)),
+                ('clf', SVC(kernel='linear'))]
+    pipeline = Pipeline(pipeline)
+    cross_val_score(pipeline, X, y, cv=skf)
