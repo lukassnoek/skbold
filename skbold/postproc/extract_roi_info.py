31674cdc3e5c026f2bbd170a2aaad4ec2863feaf
Lukas Snoek <lukassnoek@gmail.com>
2018-12-20 18:10:27 +0100

    simplify (but still ugly)

--- skbold/postproc/extract_roi_info.py
+++ skbold/postproc/extract_roi_info.py
@@ -16,10 +16,8 @@ from glob import glob
 import os.path as op
 import pandas as pd
 from scipy.ndimage.measurements import label
-
-import skbold
-roi_dir = op.join(op.dirname(skbold.__file__), 'data', 'ROIs',
-                  'harvard_oxford')
+from nilearn.datasets import fetch_atlas_harvard_oxford, load_mni152_template
+from nilearn.image import coord_transform
 
 
 def extract_roi_info(statfile, stat_name=None, roi_type='unilateral',
@@ -73,7 +71,7 @@ def extract_roi_info(statfile, stat_name=None, roi_type='unilateral',
         Dataframe corresponding to the written csv-file.
     """
 
-    if isinstance(statfile, (str, unicode)):
+    if isinstance(statfile, str):
         data = nib.load(statfile).get_data()
     else:
         data = statfile
@@ -81,101 +79,88 @@ def extract_roi_info(statfile, stat_name=None, roi_type='unilateral',
     sign_mask = np.ones(shape=data.shape)
     sign_mask[data < 0] = -1
 
-    # data = np.abs(data)
+    mni_affine = load_mni152_template().affine
 
     if stat_threshold:
         data[data < stat_threshold] = 0
 
-    masks = glob(op.join(roi_dir, roi_type, '*.nii.gz'))
+    if roi_type == 'unilateral':
+        cort_rois = fetch_atlas_harvard_oxford('cort-maxprob-thr0-2mm', symmetric_split=True)
+        subc_rois = fetch_atlas_harvard_oxford('sub-maxprob-thr0-2mm', symmetric_split=True)
+    else:
+        cort_rois = fetch_atlas_harvard_oxford('cort-maxprob-thr0-2mm', symmetric_split=True)
+        subc_rois = fetch_atlas_harvard_oxford('sub-maxprob-thr0-2mm', symmetric_split=True)
+
+    cort_rois['idx'] = np.arange(len(cort_rois['labels']))
+    subc_rois['idx'] = np.arange(len(subc_rois['labels']))
 
     df_list = []
 
-    if per_cluster:
+    # Start clustering of data
+    clustered, _ = label(data > 0)
+    values, counts = np.unique(clustered.ravel(), return_counts=True)
+    n_clust = np.argmax(np.sort(counts)[::-1] < min_clust_size)
 
-        # Start clustering of data
-        if cluster_engine == 'scipy':
-            clustered, num_clust = label(data > 0)
-            values, counts = np.unique(clustered.ravel(), return_counts=True)
-            n_clust = np.argmax(np.sort(counts)[::-1] < min_clust_size)
+    if n_clust == 0:
+        print('No (sufficiently large) clusters!')
+        return 0
 
-            if n_clust == 0:
-                print('No (sufficiently large) clusters!')
-                return 0
+    # Sort and trim
+    cluster_nrs = values[counts.argsort()[::-1][:n_clust]]
+    cluster_nrs = np.delete(cluster_nrs, 0)
 
-            # Sort and trim
-            cluster_nrs = values[counts.argsort()[::-1][:n_clust]]
-            cluster_nrs = np.delete(cluster_nrs, 0)
-        elif cluster_engine == 'fsl':
-            # Not yet implemented
-            pass
+    print('Analyzing %i clusters for %s' % (len(cluster_nrs), stat_name))
 
-        print('Analyzing %i clusters for %s' % (len(cluster_nrs), stat_name))
+    cluster_list = []
+    # Looping over clusters
+    for i, cluster_id in enumerate(cluster_nrs):
 
-        cluster_list = []
-        # Looping over clusters
-        for i, cluster_id in enumerate(cluster_nrs):
+        if verbose:
+            print('Processing cluster %i' % (i+1))
 
-            if verbose:
-                print('Processing cluster %i' % (i+1))
+        cl_mask = clustered == cluster_id
+        k = cl_mask.sum()
+        mx = data[cl_mask].max()
 
-            cl_mask = clustered == cluster_id
-            k = cl_mask.sum()
-            mx = data[cl_mask].max()
+        tmp = np.zeros(data.shape)
+        tmp[cl_mask] = data[cl_mask] == mx
 
-            tmp = np.zeros(data.shape)
-            tmp[cl_mask] = data[cl_mask] == mx
+        # in case of multiple voxels with same stat / weight
+        if np.sum(tmp == 1) > 1:
+            X, Y, Z = [coord[0] for coord in np.where(tmp == 1)]
+        else:
+            X, Y, Z = np.where(tmp == 1)
 
-            # in case of multiple voxels with same stat / weight
-            if np.sum(tmp == 1) > 1:
-                X = np.where(tmp == 1)[0][0]
-                Y = np.where(tmp == 1)[1][0]
-                Z = np.where(tmp == 1)[2][0]
-            else:
-                X = np.where(tmp == 1)[0]
-                Y = np.where(tmp == 1)[1]
-                Z = np.where(tmp == 1)[2]
+        # if weight / stat is negative, change sign of mx
+        if sign_mask[X, Y, Z] < 0:
+            mx = -mx
 
-            # if weight / stat is negative, change sign of mx
-            if sign_mask[X, Y, Z] < 0:
-                mx = -mx
+        # convert to MNI-coordinates
+        X, Y, Z = coord_transform(X, Y, Z, mni_affine)
 
-            # convert to MNI-coordinates
-            X = 90 - X * 2
-            Y = -126 + Y * 2
-            Z = -72 + Z * 2
+        # This is purely for formatting issues
+        if i == 0:
+            c = stat_name   
+        else:
+            c = ''
 
-            # This is purely for formatting issues
-            if i == 0:
+        to_append = {'Contrast': c, 'cluster': (i+1), 'k cluster': str(k),
+                        'max cluster': mx, 'x': str(X[0]), 'y': str(Y[0]),
+                        'z': str(Z[0]),
+                        'Region': '', 'k region': '', 'max region': ''}
 
-                if stat_name is None:
-                    stat = op.basename(statfile).split('.')[0].split('_')[-1]
-                    c = op.basename(op.dirname(statfile)).split('.')[0]
-                    c += '_%s' % stat
-                else:
-                    c = stat_name
-            else:
-                c = ''
+        cluster_list.append(to_append)
 
-            to_append = {'Contrast': c, 'cluster': (i+1), 'k cluster': str(k),
-                         'max cluster': mx, 'x': str(X[0]), 'y': str(Y[0]),
-                         'z': str(Z[0]),
-                         'Region': '', 'k region': '', 'max region': ''}
+        roi_list = []
+        # Loop over ROIs
+        for i_atlas, atlas in enumerate([cort_rois, subc_rois]):
+            maps = atlas['maps'].get_data()
 
-            cluster_list.append(to_append)
-
-            roi_list = []
-            # Loop over ROIs
-            for imask, mask in enumerate(masks):
-                mask_name = op.basename(mask).split('.')[0].split('_')
-                if roi_type == 'unilateral':
-                    mask_name[0] += '.'
-
-                mask_name = ' '.join(mask_name)
-
-                roi_mask = nib.load(mask).get_data() > mask_threshold
-                overlap = cl_mask.astype(int) + roi_mask.astype(int) == 2
+            for ii_roi, mask_name in enumerate(atlas['labels']):
+                roi_mask = maps == atlas['idx'][ii_roi]
+                overlap = (cl_mask.astype(int) + roi_mask.astype(int)) == 2
                 k = overlap.sum()
-
+                
                 if k > 0:
                     mx = data[overlap].max()
                     tmp = np.zeros(data.shape)
@@ -183,41 +168,33 @@ def extract_roi_info(statfile, stat_name=None, roi_type='unilateral',
 
                     # in case of multiple voxels with same stat / weight
                     if np.sum(tmp == 1) > 1:
-                        X = np.where(tmp == 1)[0][0]
-                        Y = np.where(tmp == 1)[1][0]
-                        Z = np.where(tmp == 1)[2][0]
+                        X, Y, Z = [coord[0] for coord in np.where(tmp == 1)]
                     else:
-                        X = np.where(tmp == 1)[0]
-                        Y = np.where(tmp == 1)[1]
-                        Z = np.where(tmp == 1)[2]
+                        X, Y, Z = np.where(tmp == 1)
 
                     # if sign of weight / stat is negative, change sign of mx
                     if sign_mask[X, Y, Z] < 0:
                         mx = -mx
 
                     # convert to MNI-coordinates
-                    X = 90 - X * 2
-                    Y = -126 + Y * 2
-                    Z = -72 + Z * 2
+                    X, Y, Z = coord_transform(X, Y, Z, mni_affine)
 
                 else:
                     # If no voxels, write some default values
                     mx = 0
                     X, Y, Z = 0, 0, 0
-
-                if imask == 0:
-
+                
+                if ii_roi == 0:
                     cluster_list[-1]['Region'] = mask_name
                     cluster_list[-1]['k region'] = k
                     cluster_list[-1]['max region'] = mx
-
                 else:
-
+                    
                     to_append = {'Contrast': '', 'cluster': (i + 1 + 0.1),
-                                 'k cluster': '', 'max cluster': '', 'x': '',
-                                 'y': '', 'z': '', 'Region': mask_name,
-                                 'k region': k, 'max region': mx}
-
+                                'k cluster': '', 'max cluster': '', 'x': '',
+                                'y': '', 'z': '', 'Region': mask_name,
+                                'k region': k, 'max region': mx}
+                    
                     if k > cluster_list[-1]['k region']:
                         to_append['Region'] = cluster_list[-1]['Region']
                         to_append['k region'] = cluster_list[-1]['k region']
@@ -250,47 +227,6 @@ def extract_roi_info(statfile, stat_name=None, roi_type='unilateral',
 
         df['cluster'] = ['' if (val % 1) != 0 else str(val)
                          for val in df['cluster']]
-
-    else:  # If not extracting info per cluster, but wholebrain
-        print('Analyzing %s' % statfile)
-        col_names = ['roi', 'k', 'max', 'mean', 'sd', 'X', 'Y', 'Z']
-        results = pd.DataFrame(columns=col_names)
-
-        # Only loop over masks
-        for mask in masks:
-            mask_name = op.basename(mask).split('.')[0].split('_')
-
-            if roi_type == 'unilateral':
-                mask_name[0] += '.'
-            mask_name = ' '.join(mask_name)
-
-            roi_mask = nib.load(mask).get_data() > mask_threshold
-            sig_mask = data > 0
-            overlap = sig_mask.astype(int) + roi_mask.astype(int) == 2
-            k = overlap.sum()
-            X, Y, Z = 0, 0, 0
-
-            if k > 0:
-                mx = data[overlap].max()
-                mean, std = data[overlap].mean(), data[overlap].std()
-                tmp = np.zeros(data.shape)
-                tmp[overlap] = data[overlap] == mx
-                if save_indices:
-                    X = 90 - np.where(tmp == 1)[0] * 2
-                    Y = -126 + np.where(tmp == 1)[1] * 2
-                    Z = -72 + np.where(tmp == 1)[2] * 2
-            else:
-                mx, mean, std = 0, 0, 0
-
-            to_append = {'roi': mask_name, 'k': k, 'max': mx, 'mean': mean,
-                         'sd': std, 'X': X, 'Y': Y, 'Z': Z}
-            to_append = pd.DataFrame(to_append, index=[0])
-            df_list.append(to_append)
-
-        df = pd.concat(df_list)
-        df = df[df.k != 0].sort_values(by='k', ascending=False)
-        df = df[['roi', 'k', 'max', 'mean', 'sd', 'X', 'Y', 'Z']]
-        df = df[df['k'] > min_clust_size]
 
     filename = op.join(op.dirname(statfile), 'roi_info_%s.csv' % stat_name)
     df.to_csv(filename, index=False, header=True, sep='\t')
